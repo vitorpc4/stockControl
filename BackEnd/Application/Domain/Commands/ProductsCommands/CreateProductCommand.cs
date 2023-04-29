86fc5b999f51b3f3dd0c77187c81315b49daa497
Vitor <vitorhlaiam@gmail.com>
2023-04-29 20:57:50 -0300

    Implementada validação no backend

--- BackEnd/Application/Domain/Commands/ProductsCommands/CreateProductCommand.cs
+++ BackEnd/Application/Domain/Commands/ProductsCommands/CreateProductCommand.cs
@@ -3,6 +3,7 @@ using FluentResults;
 using MediatR;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,11 +12,24 @@ namespace Application.Domain.Commands.ProductsCommands
 {
     public class CreateProductCommand : IRequest<Result<Products>>
     {
+        [Required]
+        [MaxLength(255)]
+        [MinLength(3, ErrorMessage = "Name must be at least three letters long.")]
         public string? Name { get; set; }
+        [Required]
+        [MaxLength(255)]
+        [MinLength(3, ErrorMessage = "Description must be at least three characters long.")]
         public string? Description { get; set; }
+        [Required]
+        [MaxLength(255)]
         public string? Type { get; set; }
+        [Required]
         public bool Status { get; set; }
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Quantity must be greater than or equal to one")]
         public int Quantity { get; set; }
+        [Required]
+        [Range(0, double.MaxValue, ErrorMessage = "Please enter valid unitValue")]
         public double UnitValue { get; set; }
     }
 }
